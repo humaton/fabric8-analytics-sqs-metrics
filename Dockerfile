380031532a7a4d21ae1e39f566f465da521eb9ef
Tomáš Hrčka <thrcka@redhat.com>
2017-08-17 13:46:50 +0200

    Minor updates

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,11 @@ RUN pip3 install --upgrade pip && pip install --upgrade wheel && \
 RUN mkdir -p ${HOME}/sqs-stats
 COPY app.py ${HOME}/sqs-stats
 RUN cd ${HOME}/sqs-stats
+
+#Configure boto library
+#RUN cp .aws/config ${HOME}/.aws/config
+#RUN cp .aws/credentials ${HOME}/.aws/credentials
+#RUN 
+
 RUN export FLASK_APP=app.py
 RUN flask run
--- app.py
+++ app.py
@@ -1,16 +1,21 @@
+import os
 from flask import Flask
 from flask import jsonify
 import boto3
 
+conn_args = {
+    'aws_access_key_id': os.getenv('AWS_SQS_ACCESS_KEY_ID'),
+    'aws_secret_access_key': os.getenv('AWS_SQS_SECRET_ACCESS_KEY'),
+    'region_name': 'us-east-1'
+}
 
 app = Flask(__name__)
-sqs = boto3.resource('sqs')
+sqs = boto3.resource('sqs',**conn_args)
 queue = None
 
 @app.route('/')
 def hello_world():
-    queue = sqs.get_queue_by_name(QueueName='STAGE_livenessFlow_v0')
-    return queue.attributes.get('ApproximateNumberOfMessages')
+    return 'Ola!'
 
 @app.route('/queue/<queuename>')
 def show_user_profile(queuename):
